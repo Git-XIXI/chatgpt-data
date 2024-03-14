81fe0bffa0ca6bcb67f929de0b1a8e9f5ba010cb
cjcxixi <1443203765@qq.com>
2024-03-14 23:34:26 +0800

    ci: 部署项目

---
+++ chatgpt-data-app/build.sh
@@ -0,0 +1,7 @@
+docker build -f ./Dockerfile -t chatgpt-api .
+
+
+docker build -f D:/new-chatgpt-data/chatgpt-data/chatgpt-data-app/Dockerfile -t chatgpt-data-app:1.0 .
+
+
+docker build -f ./Dockerfile -t chatgpt-data:1.1 .
