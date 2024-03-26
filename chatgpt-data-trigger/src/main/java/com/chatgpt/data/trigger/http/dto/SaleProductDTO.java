ad012e2ae434c7c514f84926911075d6a1847d35
cjcxixi <1443203765@qq.com>
2024-03-26 23:47:07 +0800

    feat: 支付成功，回调消息，变更订单状态和发货

---
+++ chatgpt-data-trigger/src/main/java/com/chatgpt/data/trigger/http/dto/SaleProductDTO.java
@@ -0,0 +1,40 @@
+package com.chatgpt.data.trigger.http.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+/**
+ * @description: 商品对象 DTO
+ * @date 2024/3/26 22:09
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SaleProductDTO {
+
+    /**
+     * 商品ID
+     */
+    private Integer productId;
+    /**
+     * 商品名称
+     */
+    private String productName;
+    /**
+     * 商品描述
+     */
+    private String productDesc;
+    /**
+     * 额度次数
+     */
+    private Integer quota;
+    /**
+     * 商品价格
+     */
+    private BigDecimal price;
+}
